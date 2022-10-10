069836a3f62f0b117dd15698df282ce2ba1fa8f8
Mostafa W@fy <113066394+Mostafa-Wafy@users.noreply.github.com>
2022-10-10 15:01:55 +0200

    Edited Configuration.h

--- Config_Frame.cpp
+++ Config_Frame.cpp
@@ -1,30 +1,23 @@
 #include <iostream>
 #include "Config_Frame.h"
 using namespace std;
-class Config_Frame{
-    private:
-        int size_Of_Frame;
-        TransportLayerProtocol enum_transport_layer_protocol;
-        NetworkLayerProtocol enum_network_layer_protocol;
-    public:
     /*default values size of frame =64 , transport protocol is UDP, Network protocol is IPv4*/
-    Config_Frame(){
+    Config_Frame::Config_Frame(){
         size_Of_Frame=64;
         enum_transport_layer_protocol=UDP;
         enum_network_layer_protocol=IPv4;
     }
-    Config_Frame(int size,TransportLayerProtocol enum_transport_layer_protocol,NetworkLayerProtocol enum_network_layer_protocol){
+    Config_Frame::Config_Frame(int size,TransportLayerProtocol enum_transport_layer_protocol,NetworkLayerProtocol enum_network_layer_protocol){
         this->size_Of_Frame=size;
         this->enum_transport_layer_protocol=enum_transport_layer_protocol;
         this->enum_network_layer_protocol=enum_network_layer_protocol;
     }
-    int getSizeOfFrame(void){return this->size_Of_Frame;}
-    TransportLayerProtocol getProtocolOfTransportLayer(void){return this->enum_transport_layer_protocol;}
-    NetworkLayerProtocol getProtocolOfNetworkLayer(void){return this->enum_network_layer_protocol;}
-    void setSizeOfFrame(int size){this->size_Of_Frame=size;}
-    void setProtocolOfTransportLayer(TransportLayerProtocol enum_transport_layer_protocol){
+    int Config_Frame::getSizeOfFrame(void){return this->size_Of_Frame;}
+    TransportLayerProtocol Config_Frame::getProtocolOfTransportLayer(void){return this->enum_transport_layer_protocol;}
+    NetworkLayerProtocol  Config_Frame::getProtocolOfNetworkLayer(void){return this->enum_network_layer_protocol;}
+    void Config_Frame::setSizeOfFrame(int size){this->size_Of_Frame=size;}
+    void Config_Frame::setProtocolOfTransportLayer(TransportLayerProtocol enum_transport_layer_protocol){
         this->enum_transport_layer_protocol=enum_transport_layer_protocol;}
-    void setProtocolOfNetworkLayer(NetworkLayerProtocol enum_network_layer_protocol){
+    void Config_Frame::setProtocolOfNetworkLayer(NetworkLayerProtocol enum_network_layer_protocol){
         this->enum_network_layer_protocol= enum_network_layer_protocol;
     }
-};
--- Config_Frame.h
+++ Config_Frame.h
@@ -8,16 +8,25 @@ typedef enum{
 typedef enum{
     IPv4,IPv6
 }NetworkLayerProtocol;
-/*return used size of frame*/
-int getSizeOfFrame(void); 
-/*return used transport layer protocal in frame*/
-TransportLayerProtocol getProtocolOfTransportLayer(void);
-/*return used network layer protocal in frame*/
-NetworkLayerProtocol getProtocolOfNetworkLayer(void);
-/*change size of frame*/
-void setSizeOfFrame(int size);
-/*change used transport layer protocal in frame*/
-void setProtocolOfTransportLayer(TransportLayerProtocol enum_transport_layer_protocol);
-/*change used network layer protocal in frame*/
-void setProtocolOfNetworkLayer(NetworkLayerProtocol enum_network_layer_protocol);
+class Config_Frame{
+    private:
+        int size_Of_Frame;
+        TransportLayerProtocol enum_transport_layer_protocol;
+        NetworkLayerProtocol   enum_network_layer_protocol;
+    public:
+    Config_Frame();
+    Config_Frame(int size,TransportLayerProtocol enum_transport_layer_protocol,NetworkLayerProtocol enum_network_layer_protocol);
+    /*return used size of frame*/
+    int getSizeOfFrame(void); 
+    /*return used transport layer protocal in frame*/
+    TransportLayerProtocol getProtocolOfTransportLayer(void);
+    /*return used network layer protocal in frame*/
+    NetworkLayerProtocol getProtocolOfNetworkLayer(void);
+    /*change size of frame*/
+    void setSizeOfFrame(int size);
+    /*change used transport layer protocal in frame*/
+    void setProtocolOfTransportLayer(TransportLayerProtocol enum_transport_layer_protocol);
+    /*change used network layer protocal in frame*/
+    void setProtocolOfNetworkLayer(NetworkLayerProtocol enum_network_layer_protocol);
+};
 #endif
