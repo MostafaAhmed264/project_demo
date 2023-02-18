7171965ee43f6f3a95ee511f64af19ef761e0f01
root <zyadfahmy2@gmail.com>
2023-02-18 12:13:30 +0200

    try

---
+++ GlobalVariable.java
@@ -0,0 +1,8 @@
+package Phase0;
+
+public class GlobalVariable {
+    public static String interfaceName = "enp2s0";
+    public static int webSocketPort = 9099;
+    public static int webSocketMaxMessages = 100000;
+
+}
---
+++ network/MacAddresses.java
@@ -0,0 +1,5 @@
+package Phase0.network;
+
+public class MacAddresses {
+    public static String Mac1 = "3C:2C:30:9B:3B:90";
+}
